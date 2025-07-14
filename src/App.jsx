22f99250a37635552071b73de4f4755f1a16965f
adityat47 <aditya.tripathi@47billion.com>
2025-07-15 00:44:07 +0530

    intial commit to new branch

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Form from './Components/Form'
+
+
+
+function App() {
+ 
+
+  return (
+   <>
+   <h1>Playground</h1>
+   <Form/>
+    
+   </>
+  )
+}
+
+export default App
